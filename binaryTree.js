5b1b6e7a0380a9288ee80fc1c6da930e94e9889d
IgnFed <ignaciofedorenco2317@gmail.com>
2021-09-14 17:30:46 -0300

    Modified/added functions

--- binaryTree.js
+++ binaryTree.js
@@ -1,95 +1,130 @@
-class Node{
-  constructor(val){
+class Node {
+  constructor(val) {
     this.val = val;
     this.left = null;
     this.right = null;
   }
 }
 
-class Tree{
-  constructor(){
+class Tree {
+  constructor() {
     this.root = null;
   }
 
   /*Function - Method*/
-  nuevoNodo(value = undefined){
-    if(!value) return "Insert A Value";
+  newNode(value = undefined) {
+    if (!value) return 'Insert A Value';
 
     const node = new Node(value);
-    if(!this.root) {
+    if (!this.root) {
       this.root = node;
-      return this.root
-    };
-    return this.comprobarPesoNodo(node)
-  };
-
-  /*Function - Method*/
-  comprobarPesoNodo(value){
-   return this.#auxComprobarPesoNodo(value)
-  }
-
-  /*Function - PRIVATE Method*/
-  #auxComprobarPesoNodo(value, root = this.root, level=1){
-      if(value.val < root.val && root.left == null ){
-        root.left = value
-        return `${'*'.repeat(level)} ${value.val} --- left of ${root.val}`;
-      }else if(value.val < root.val && root.left) return this.#auxComprobarPesoNodo(value, root.left, level+1);
-
-      if(value.val > root.val && root.right == null ){
-        root.right = value
-        return `${'*'.repeat(level)} ${value.val} --- right of ${root.val}`;
-      }else if(value.val > root.val && root.right) return this.#auxComprobarPesoNodo(value, root.right, level+1);
-
-      return ["null"];
+    }
+    this.checkWeightNode(node);
   }
 
   /*Function - Method*/
-  buscarNodo(value){
-    return this.#auxBuscarNodo(value);
+  checkWeightNode(value) {
+    return this.#auxCheckWeightNode(value);
+  }
+
+  /*Function - Method*/
+  getRoot() {
+    return this.root;
   }
 
   /*Function - PRIVATE Method*/
-  #auxBuscarNodo(value, root=this.root){
-      if(value == root.val) return root;
-      if(value < root.val && root.left) return this.#auxBuscarNodo(value, root.left);
-      if(value > root.val  && root.right) return this.#auxBuscarNodo(value, root.right);
-      return `No se ha encontrado el valor: ${value}.` ;
+  #auxCheckWeightNode(value, root = this.root, level = 1) {
+    if (value.val < root.val && root.left == null) {
+      root.left = value;
+      return `${'*'.repeat(level)} ${value.val} --- left of ${root.val}`;
+    } else if (value.val < root.val && root.left)
+      return this.#auxCheckWeightNode(value, root.left, level + 1);
+
+    if (value.val > root.val && root.right == null) {
+      root.right = value;
+      return `${'*'.repeat(level)} ${value.val} --- right of ${root.val}`;
+    } else if (value.val > root.val && root.right)
+      return this.#auxCheckWeightNode(value, root.right, level + 1);
+
+    return ['null'];
   }
 
+  /*Function - Method*/
+  SearchNode(value) {
+    return this.#auxSearchNode(value);
+  }
+
+  /*Function - PRIVATE Method*/
+  #auxSearchNode(value, root = this.root) {
+    if (value == root.val) return root;
+    if (value < root.val && root.left)
+      return this.#auxSearchNode(value, root.left);
+    if (value > root.val && root.right)
+      return this.#auxSearchNode(value, root.right);
+    return `No se ha encontrado el valor: ${value}.`;
+  }
 
   /*Function - Method*/
 
-  recorrer(type = 1){
-    if(type == -1) return this.#postOrden()
-    if(type == 1) return this.#preOrden()
+  travel(type = 1, fn) {
+    if (type == 0) this.#inOrder(this.root, 1, '', fn);
+    if (type == 1) this.#preOrder(this.root, 1, '', fn);
+    if (type == -1) this.#postOrder(this.root, 1, '', fn);
   }
 
   /*Function - PRIVATE Method*/
-  #preOrden(root = this.root, level=1 ){
-    console.log(`${'*'.repeat(level)} ${root.val}`)
-    if(root.left != null) this.#preOrden(root.left, level+1);
-    if(root.right != null) this.#preOrden(root.right, level+1);
+  #preOrder(root, level, type, fn) {
+    fn(root, level, type);
+    if (root.left != null) this.#preOrder(root.left, level + 1, '<', fn);
+    if (root.right != null) this.#preOrder(root.right, level + 1, '>', fn);
   }
 
   /*Function - PRIVATE Method*/
-  #postOrden(root = this.root, level=1 ){
-    console.log(`${'*'.repeat(level)} ${root.val}`)
-     if(root.right != null) this.#postOrden(root.right, level+1 )
-     if(root.left != null) this.#postOrden(root.left, level+1)
+  #postOrder(root, level, type, fn) {
+    if (root.left != null) this.#postOrder(root.left, level + 1, '<', fn);
+    if (root.right != null) this.#postOrder(root.right, level + 1, '>', fn);
+    fn(root, level, type);
   }
 
+  #inOrder(root, level, type, fn) {
+    if (root.left != null) this.#inOrder(root.left, level + 1, '<', fn);
+    fn(root, level, type);
+    if (root.right != null) this.#inOrder(root.right, level + 1, '>', fn);
+  }
 }
 
 const tree = new Tree();
-tree.nuevoNodo(23);
-tree.nuevoNodo(12);
-tree.nuevoNodo(42);
-tree.nuevoNodo(47);
-tree.nuevoNodo(10);
-tree.nuevoNodo(-12);
+tree.newNode(45);
+tree.newNode(23);
+tree.newNode(65);
+tree.newNode(52);
+tree.newNode(96);
+tree.newNode(48);
+tree.newNode(2);
+tree.newNode(38);
+tree.newNode(7);
 
-"PRE-ORDEN"
- tree.recorrer(1)
+('IN-ORDER');
+tree.travel(0, function (root, level, type) {
+  console.log(`${'--'.repeat(level)}${type} ${root.val}`);
+});
 
-"POST-ORDEN"
-tree.recorrer(-1)
+(() => {
+  setTimeout(() => {
+    console.log('PRE-ORDEN');
+    tree.travel(1, function (root, level) {
+      console.log(`${'--'.repeat(level)} ${root.val}`);
+    });
+    return clearTimeout();
+  }, 500);
+})();
+
+(() => {
+  setTimeout(() => {
+    console.log('POST-ORDEN');
+    tree.travel(-1, function (root, level) {
+      console.log(`${'--'.repeat(level)} ${root.val}`);
+    });
+    return clearTimeout();
+  }, 1000);
+})();
